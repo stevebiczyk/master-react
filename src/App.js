2ccf08703c96f42d51f188d3f738a84b8dfb6081
stevebiczyk <stevebiczyk@gmail.com>
2024-01-06 23:20:19 +0000

    expressions in JSX

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import Add from "./components/Add";
 import Greetings from "./components/Greetings";
 
 function App() {
+  const myName = "Istvan Biczyk";
+  const multiply = (a, b) => a * b;
+  const specialClass = "simple-class";
+
   return (
     <>
-      <Greetings />
-      <Add />
+      <h1>{myName}</h1>
+      <p> 2 * 2 = {2 * 2}</p>
+      <p> My Friends List: {["Alex, Andy, Keith"]}</p>
+      <p> 2 * 2 = {multiply(2, 2)}</p>
+      <p className={specialClass}>This is a simple class</p>
     </>
   );
 }
