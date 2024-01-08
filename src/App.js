5f002b77bb978180412f608e4744e0efd8281d75
stevebiczyk <stevebiczyk@gmail.com>
2024-01-08 21:44:28 +0000

    Lists in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import Add from "./components/Add";
-import Greetings from "./components/Greetings";
-
 function App() {
-  const myName = "Istvan Biczyk";
-  const multiply = (a, b) => a * b;
-  const specialClass = "simple-class";
-
+  const userInfo = [
+    {
+      name: "John Doe",
+      email: "test1@gmail.com",
+      location: "United States",
+    },
+    {
+      name: "Steve Smith",
+      email: "test2@gmail.com",
+      location: "United Kingdom",
+    },
+    {
+      name: "Jane Doe",
+      email: "test3@gmail.com",
+      location: "United States",
+    },
+  ];
   return (
     <>
-      <h1>{myName}</h1>
-      <p> 2 * 2 = {2 * 2}</p>
-      <p> My Friends List: {["Alex, Andy, Keith"]}</p>
-      <p> 2 * 2 = {multiply(2, 2)}</p>
-      <p className={specialClass}>This is a simple class</p>
+      {userInfo.map((user) => (
+        <ul key={Math.random() * 10}>
+          <li>{user.name}</li>
+          <li>{user.email}</li>
+          <li>{user.location}</li>
+        </ul>
+      ))}
     </>
   );
 }
